0bd5b897ba8f373d5341d05f6e81408dd12ea133
iampenkin@gmail.com <54068102+penkin-repo@users.noreply.github.com>
2025-08-18 01:50:28 +0300

    fix: supabase.ts

--- lib/supabase.template.ts
+++ lib/supabase.template.ts
@@ -1,11 +1,17 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { Database } from './typesSupabase';
 
-// Эти значения заменятся при сборке
-const supabaseUrl: string = '{{SUPABASE_URL}}';
-const supabaseAnonKey: string = '{{SUPABASE_ANON_KEY}}';
+const supabaseUrl: string = '${{ secrets.SUPABASE_URL }}';
+const supabaseAnonKey: string = '${{ secrets.SUPABASE_ANON_KEY }}';
+
+export const SUPABASE_CONFIG_ERROR = supabaseUrl === '' || supabaseAnonKey === '';
 
 let supabase: SupabaseClient<Database> | null = null;
-supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+if (!SUPABASE_CONFIG_ERROR) {
+  supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+} else {
+  console.warn('Supabase credentials are missing!');
+}
 
 export { supabase };
